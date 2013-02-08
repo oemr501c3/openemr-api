dc396b45d7c88a9131085ca42a8e156eea868398
medmasterpro <iqbal@medmastermobile.com>
2013-02-08 13:12:48 +0500

    Modified test environment configuration and use global configuration

--- medmasterproapi/openemr/includes/configure.php
+++ medmasterproapi/openemr/includes/configure.php
@@ -1,18 +1,13 @@
 <?php
-	//error_reporting(1);
-	set_time_limit(0);
-
-	// define our database connection
-//	define('DB_SERVER', '71.95.199.108:3306'); // eg, localhost - should not be empty for productive servers
-//	define('DB_USERNAME', 'karl');
-//	define('DB_PASSWORD', 'crackers1');
-//	define('DB_DATABASE', 'openemr');
-//        
-        define('DB_SERVER', '184.72.52.108:3306'); // eg, localhost - should not be empty for productive servers
-	define('DB_USERNAME', 'admin');
-	define('DB_PASSWORD', 'kre52865');
-	define('DB_DATABASE', 'openemr1');
-	  
+	
+	set_time_limit(0);	
+        extract($GLOBALS['sqlconf']);
+        
+        define('DB_SERVER', $host); // eg, localhost - should not be empty for productive servers
+	define('DB_USERNAME', $login);
+	define('DB_PASSWORD', $pass);
+	define('DB_DATABASE', $dbase);
+        
 	// ezSQL Constants
 	define("EZSQL_VERSION","1.26");
 	define("OBJECT","OBJECT",true);
